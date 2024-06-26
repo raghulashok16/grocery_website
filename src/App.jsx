d692808919ce03ae13232832457ad490495699f7
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-06-26 16:47:24 +0530

    install router-dom and add router pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './router/HomePage';
+import ProductsPage from './router/ProductsPage';
+import PageNotFound from './router/PageNotFound';
+import CartPage from './router/CartPage';
+import OrdersPage from './router/OrdersPage';
 
 function App() {
 
   return (
     <>
-      <h1 className=''>Tailwind project</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/products' element={<ProductsPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/orders' element={<OrdersPage />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
     </>
   )
 }
